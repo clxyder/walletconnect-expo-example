d7ff225067c0b7b6a7a311a7381947dd4508ce91
Ibrahim <boudchicha.ibrahim@gmail.com>
2022-05-22 19:22:47 +0100

    require global.ts when platform is not web

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 /* eslint-disable eslint-comments/no-unlimited-disable */
 /* eslint-disable */
 
-import './global';
+const { Platform } = require("react-native");
 
-const { registerRootComponent, scheme } = require('expo');
-const { default: App } = require('./App');
+if (Platform.OS !== "web") {
+  require("./global");
+}
 
-const { default: AsyncStorage } = require('@react-native-async-storage/async-storage');
-const { withWalletConnect } = require('@walletconnect/react-native-dapp');
+const { registerRootComponent, scheme } = require("expo");
+const { default: App } = require("./App");
+
+const {
+  default: AsyncStorage,
+} = require("@react-native-async-storage/async-storage");
+const { withWalletConnect } = require("@walletconnect/react-native-dapp");
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(withWalletConnect(App, {
-    redirectUrl: Platform.OS === 'web' ? window.location.origin : `${scheme}://`,
+registerRootComponent(
+  withWalletConnect(App, {
+    redirectUrl:
+      Platform.OS === "web" ? window.location.origin : `${scheme}://`,
     storageOptions: {
       asyncStorage: AsyncStorage,
     },
-  }));
+  })
+);
